43fa091efbf5d318564108940b0683322576279c
Aditya Bhatt <31549csds@gmail.com>
2026-02-24 10:40:45 +0530

    Initial Commit

---
+++ FitMacroAI/FitMacroAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMacroAIApp.swift
+//  FitMacroAI
+//
+//  Created by Aditya Bhatt on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FitMacroAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
